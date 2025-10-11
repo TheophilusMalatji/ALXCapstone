d63b57d13c96e31f6b2c44be9aa0b56f692799af
TheophilusMalatji <theo.malatji@gmail.com>
2025-10-11 14:43:48 +0200

    Restart

--- Vue-Front-End/src/lib/stores.js
+++
@@ -1,8 +0,0 @@
-import { writable } from "svelte/store";
-
-export const sidebarStatus = writable(true);
-export const allMaps = writable([]);
-export const activeMap = writable();
-export const years = writable([]);
-export const activeYear = writable();
-export const headerData = writable();
--- Vue-Front-End/src/routes/api/header.json.js
+++
@@ -1,7 +0,0 @@
-// @ts-nocheck
-export async function GET() {
-    const header = await import("../../../contentrain/header/header.json");
-    return { body: header.default }
-   
-    
-  }
